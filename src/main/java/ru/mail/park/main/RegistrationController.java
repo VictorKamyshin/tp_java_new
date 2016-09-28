ab6f23bb7239c22a9c8677fc49dc61aa0bb79fa8
Victor <Kamyshin.Victor@yandex.ru>
2016-09-28 15:24:45 +0300

    front_rk

--- src/main/java/ru/mail/park/main/RegistrationController.java
+++ src/main/java/ru/mail/park/main/RegistrationController.java
@@ -1,5 +1,7 @@
 package ru.mail.park.main;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,6 +9,7 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import ru.mail.park.model.UserProfile;
 import ru.mail.park.services.AccountService;
+import ru.mail.park.services.SessionService;
 
 import javax.servlet.http.HttpSession;
 
@@ -15,17 +18,23 @@ import javax.servlet.http.HttpSession;
  */
 
 @RestController
-public class RegistrationController {
+public class RegistrationController{
     private final AccountService accountService;
+    private final SessionService sessionService;
 
     @Autowired
-    public RegistrationController(AccountService accountService) {
+    public RegistrationController(AccountService accountService, SessionService sessionService) {
         this.accountService = accountService;
+        this.sessionService = sessionService;
     }
 
     @RequestMapping(path = "/api/user/", method = RequestMethod.POST)
     public ResponseEntity login(@RequestBody RegistrationRequest body,
                                 HttpSession httpSession) {
+//    public ResponseEntity login(@RequestParam String login,
+//                                @RequestParam String password,
+//                                @RequestParam String email,
+//                                HttpSession httpSession) {
         final String sessionId = httpSession.getId();
 
         final String login = body.getLogin();
@@ -34,29 +43,64 @@ public class RegistrationController {
         if (StringUtils.isEmpty(login)
                 || StringUtils.isEmpty(password)
                 || StringUtils.isEmpty(email)) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{}");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Один из параметров пуст");
         }
         final UserProfile existingUser = accountService.getUser(login);
         if (existingUser != null) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{}");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Такой пользователь уже существует");
         }
-
+        final String existingLogin = sessionService.getLogin(sessionId);
+        if(existingLogin!=null) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Вы уже авторизированы");
+        }
         accountService.addUser(login, password, email);
-        return ResponseEntity.ok(new SuccessResponse(login));
+        return ResponseEntity.ok(new SuccessResponse("Пользователь успешно создан"));
+    }
+
+    @RequestMapping(path = "/hello", method = RequestMethod.GET)
+    public ResponseEntity hello(HttpSession httpSession) {
+        final String sessionId = httpSession.getId();
+        String login = sessionService.getLogin(sessionId);
+        if(login!=null) {
+            return ResponseEntity.ok(new SuccessResponse("Вы авторизованы"));
+        } else {
+            return ResponseEntity.ok(new SuccessResponse("Вы не авторизованы"));
+        }
+    }
+
+    @RequestMapping(path = "/exit", method = RequestMethod.GET)
+    public ResponseEntity exit(HttpSession httpSession) {
+        final String sessionId = httpSession.getId();
+        String login = sessionService.removeLogin(sessionId);
+        if(login != null) {
+            return ResponseEntity.ok(new SuccessResponse("Вы больше не авторизованы"));
+        } else {
+            return ResponseEntity.ok(new SuccessResponse("Вы не были авторизованы"));
+        }
     }
 
     @RequestMapping(path = "/api/session", method = RequestMethod.POST)
-    public ResponseEntity auth(@RequestParam(name = "login") String login,
-                               @RequestParam(name = "password") String password) {
+    public ResponseEntity auth(@RequestBody AuthorisationRequest body,
+                               HttpSession httpSession) {
+        final String sessionId = httpSession.getId();
+
+        final String login = body.getLogin();
+        final String password = body.getPassword();
         if (StringUtils.isEmpty(login)
                 || StringUtils.isEmpty(password)) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{}");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Некорректный запрос");
         }
         final UserProfile user = accountService.getUser(login);
-        if (user.getPassword().equals(password)) {
-            return ResponseEntity.ok(new SuccessResponse(user.getLogin()));
+        if (user == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Такого пользователя не существует");
         }
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{}");
+        if (user.getPassword().equals(password)) {
+            final String existingLogin = sessionService.getLogin(sessionId);
+            if(existingLogin!=login) { sessionService.removeLogin(sessionId); }
+            sessionService.addSession(sessionId, login);
+            return ResponseEntity.ok(new SuccessResponse("Вы успешно авторизованы"));
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Что-то пошло не так");
     }
 
     private static final class RegistrationRequest {
@@ -64,7 +108,10 @@ public class RegistrationController {
         private String password;
         private String email;
 
-        private RegistrationRequest(String login, String password, String email) {
+        @JsonCreator
+        private RegistrationRequest(@JsonProperty("login") String login,
+                                    @JsonProperty("password") String password,
+                                    @JsonProperty("email") String email) {
             this.login = login;
             this.password = password;
             this.email = email;
@@ -83,16 +130,37 @@ public class RegistrationController {
         }
     }
 
-    private static final class SuccessResponse {
+    private static final class AuthorisationRequest {
         private String login;
+        private String password;
+
+        @JsonCreator
+        private AuthorisationRequest(@JsonProperty("login") String login,
+                                    @JsonProperty("password") String password) {
+            this.login = login;
+            this.password = password;
+        }
+
+        public String getLogin() {
+            return login;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+    }
+
+    private static final class SuccessResponse {
+        private String responce;
 
         private SuccessResponse(String login) {
-            this.login = login;
+            this.responce = login;
         }
 
         @SuppressWarnings("unused")
         public String getLogin() {
-            return login;
+            return responce;
         }
     }
 
--- src/main/java/ru/mail/park/services/SessionService.java
+++ src/main/java/ru/mail/park/services/SessionService.java
@@ -2,11 +2,25 @@ package ru.mail.park.services;
 
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * ДЗ: реализовать класс, который по соответствующему id сессии будет отдавать пользователей(т.е. реализовать авторизацию по сессии)
  *
  */
 @Service
 public class SessionService {
+    private Map<String, String> SessionIdTologin = new HashMap<>();
+
+    public String addSession(String sessionId, String login) {
+        return SessionIdTologin.put(sessionId, login);
+    }
+
+    public String getLogin(String sessionId) {
+        return SessionIdTologin.get(sessionId);
+    }
+
+    public String removeLogin(String sessionId) { return SessionIdTologin.remove(sessionId); }
     //To be done
 }
